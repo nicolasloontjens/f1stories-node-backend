0e721a3bfde6b5046ee614f9eed6cf2f2f39b9d8
NicolasLoontjens <nicolas.loontjens@student.howest.be>
2022-04-10 14:58:24 +0200

    added /races endpoint with pug template

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 let express = require('express');
 let router = express.Router();
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+const url = "http://127.0.0.1:3001/api";
+chai.use(chaiHttp);
+
 
 /* GET home page. */
-router.get('/races', function(req, res) {
-  res.render('index');
+router.get('/races',async function(req, res) {
+  const result = await chai.request(url).get('/races').send();
+  let races = result.body;
+  console.log(races);
+  races = races.map(race => {
+    let date = new Date(race.date);
+    let currdate = new Date();
+    race.date = `Date: ${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+    if(currdate > date){
+      race.hasHappened = true;
+    }else{
+      race.hasHappened = false;
+    }
+    return race;
+  })
+  console.log(races);
+  res.render('index', {'races':races,'title':'Races'});
 });
 
 module.exports = router;
